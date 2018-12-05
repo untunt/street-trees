36b7c24cd3e3fe6327a3be25df604b000ca69646
unt <tw1701@nyu.edu>
2018-12-05 03:59:32 -0500

    feat: add Number of Trees

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # StreetTrees!
+
 **Authors**: Tianheng Wang (tw1701)
 
 Group 22
 
 ![Screenhot](screenshot.jpg)
 
-
 ## About
-In this project we ...
 
-## More
-[DEMO](https://nyu-vis-fall2018.github.io/project-template/)
+In this project, I tell the story of street trees in NYC, by visualizing multiple features of street trees and their relationship with species, location and street suffixes.
 
-[Project Descrioption](project.pdf)
+[DEMO](https://phesoca.com/street-trees/)
+
+## Data Set
+
+[2015 Street Tree Census](https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/uvpi-gqnh) is conducted by volunteers and staff organized by NYC Parks & Recreation and partner organizations. In the data set, there are tree data collected includes tree species, diameter and perception of health.
 
 ## Files
 
-[2010 Census Tracts.geojson](2010 Census Tracts.geojson) is the shape data of the census tract, a geographic division used by the U.S. Census. It can be found [here](https://data.cityofnewyork.us/City-Government/2010-Census-Tracts/fxpq-c8ku) (to download, see "Export").
+[tracts.geo.json](data/tracts.geo.json) is the shape data of the census tract, a geographic division used by the U.S. Census. It can be found at [2010 Census Tracts](https://data.cityofnewyork.us/City-Government/2010-Census-Tracts/fxpq-c8ku) (to download, see "Export").
--- func.js
+++ func.js
@@ -1,41 +1,33 @@
 let tracts;
 let numberByCT;
 let numberBySpecies;
-loadData().then(showData);
 
 function total(record) {
     return parseInt(record.Fair) + parseInt(record.Good) + parseInt(record.Poor);
 }
 
-function loadData() {
+function loadData(dir) {
     return Promise.all([
-        d3.json("data/tracts.geo.json"),
-        d3.csv("data/num_by_ct.csv"),
-        d3.csv("data/num_by_spc.csv")
+        d3.json(`${dir}tracts.geo.json`),
+        d3.csv(`${dir}num_by_ct.csv`),
+        d3.csv(`${dir}num_by_spc.csv`)
     ]).then(datasets => {
         tracts = datasets[0];
         numberByCT = datasets[1];
         numberBySpecies = datasets[2];
     });
 }
+
 function showData() {
-    //console.log(tracts.features[2])
-    //console.log(numberBySpecies[2])
-    
     let width = 600;
     let height = 400;
     let width_map = 800;
-    let margin = {
-        top: 10,
-        bottom: 30,
-        left: 100,
-        right: 10
-    };
-    let bodyHeight = height - margin.top - margin.bottom;
-    let bodyWidth = width - margin.left - margin.right;
-    
     let green = "rgb(20, 108, 54)";
     let yellow = "rgb(244, 209, 102)";
+    // Set an array of colors from yellow to green
+    var color = d3.scaleLinear()
+        .domain([0, 100])
+        .range([yellow, green]);
     
     let container;
     let projection = d3.geoMercator();
@@ -44,14 +36,58 @@ function showData() {
         .translate([width_map / 2, width_map / 2]);
     let path = d3.geoPath(projection);
     
+    let margin = {
+        top: 10,
+        bottom: 30,
+        left: 70,
+        right: 10
+    };
+    let bodyHeight = height - margin.top - margin.bottom;
+    let bodyWidth = width - margin.left - margin.right;
+    let xScale;
+    let yScale;
+    
+    container = d3.select("#count");
+    container
+        .attr("width", width)
+        .attr("height", height)
+    let boroName = ["0", "Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"];
+    let boroCount = [0, 0, 0, 0, 0, 0];
+    numberByCT.forEach(a => {
+        // The first digit is borough numeric code
+        boroCount[a.boro_ct[0]] += total(a);
+    });
+    xScale = d3.scaleBand()
+        .range([0, bodyWidth])
+        .domain(boroName.slice(1))
+        .padding(0.1);
+    yScale = d3.scaleLinear()
+        .range([bodyHeight, 0])
+        .domain([0, d3.max(boroCount)]);
+    container.append("g")
+        .style("transform", `translate(${margin.left}px, ${margin.top}px)`)
+        .selectAll(".bar")
+        .data(boroCount.slice(1))
+        // Adding a rect tag for each borough
+        .enter().append("rect")
+        .attr("y", d => yScale(d))
+        .attr("height", d => yScale(0) - yScale(d))
+        .attr("x", (d, i) => xScale(boroName[i + 1]))
+        .attr("width", xScale.bandwidth())
+        .attr("fill", color(80));
+    container.append("g")
+        .style("transform", `translate(${margin.left}px, ${height - margin.bottom}px)`)
+        .call(d3.axisBottom(xScale).ticks(5));
+    container.append("g")
+        .style("transform", `translate(${margin.left}px, ${margin.top}px)`)
+        .call(d3.axisLeft(yScale));
+    document.getElementById('sum').innerHTML = d3.sum(boroCount).toLocaleString();
+
     container = d3.select("#density");
+    margin.left = 120;
     container
         .attr("width", width_map)
         .attr("height", width_map)
-    // Set an array of colors from yellow to green
-    var color = d3.scaleLinear()
-        .domain([0, 100])
-        .range([yellow, green]);
     container.selectAll("path").data(tracts.features)
         .enter().append("path")
         .attr("d", path) // Use the path generator to draw each tract
@@ -66,9 +102,14 @@ function showData() {
             }
             number = total(numberByCTRow);
             density = number / parseFloat(d.properties.shape_area);
-            // Set color accoring to the density. The max of density is 0.000265
+            // Set color accoring to the density
+            // The min to show is 0.000015
+            if (density < 0.000015) {
+                return color(0);
+            }
+            // The max of density is 0.000265
             // A 0.75 exponent is set so that there will be more green (data near 1)
-            return color(Math.floor(Math.pow(density / 0.000265, 0.75)  * 100));
+            return color(Math.floor(Math.pow((density - 0.000015) / 0.000265, 0.75)  * 100));
         })
     
     container = d3.select("#species");
@@ -78,11 +119,11 @@ function showData() {
     numberBySpecies.sort((a, b) => total(b) - total(a));
     // Display top 10 species only
     top10 = numberBySpecies.slice(0, 10);
-    let xScale = d3.scaleLinear()
+    xScale = d3.scaleLinear()
         .range([0, bodyWidth])
         // Max of count 
         .domain([0, total(top10[0])]);
-    let yScale = d3.scaleBand()
+    yScale = d3.scaleBand()
         .range([0, bodyHeight])
         // Covert to a list of species
         .domain(top10.map(a => a.spc_common))
@@ -96,11 +137,11 @@ function showData() {
         .attr("height", yScale.bandwidth())
         .attr("y", d => yScale(d.spc_common))
         .attr("width", d => xScale(total(d)))
-        .attr("fill", color(80))
+        .attr("fill", color(80));
     container.append("g")
         .style("transform", `translate(${margin.left}px, ${height - margin.bottom}px)`)
-        .call(d3.axisBottom(xScale).ticks(5))
+        .call(d3.axisBottom(xScale).ticks(5));
     container.append("g")
         .style("transform", `translate(${margin.left}px, ${margin.top}px)`)
-        .call(d3.axisLeft(yScale))
+        .call(d3.axisLeft(yScale));
 }
